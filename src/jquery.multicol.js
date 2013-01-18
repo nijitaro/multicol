7564d71bae577e39e1449979408c29d4443654a4
Kotaro Imai <mail@hokypoky.info>
2013-01-18 10:54:16 +0900

    ver 1.2.1

--- src/jquery.multicol.js
+++ src/jquery.multicol.js
@@ -1,8 +1,8 @@
 /**
  * @name multicol.js
  * @fileOverview
- * @version 1.2
- * @author <a href="mailto:imai@4digit.jp">Kotaro Imai</a> @ <a href="http://hokypoky.info">HOKYPOKY.</a>
+ * @version 1.2.1
+ * @author <a href="mailto:mail@hokypoky.info">Kotaro Imai</a> @ <a href="http://hokypoky.info">HOKYPOKY.</a>
  * @description
  * <p>WEBサイトで段組み行うことができるjQuery Plugin</p>
  * <p>(c) HOKYPOKY.info Licensed <a href="http://ja.wikipedia.org/wiki/GNU_General_Public_License">GNU General Public License</a>.</p>
